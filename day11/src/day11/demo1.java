b05c9f016683caa2bd6be66678f7769e14e5be87
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-22 08:25:17 +0100

    day 11 done

---
+++ day11/src/day11/demo1.java
@@ -0,0 +1,18 @@
+package day11;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		String str = "Programming";
+
+		String s = new String("World");
+
+		System.out.println(str);
+
+		System.out.println(s);
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
