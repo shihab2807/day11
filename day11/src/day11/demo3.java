6183e2b5f6db773406cb02a532e67c4ea776b10c
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-22 08:29:32 +0100

    day11 done

---
+++ day11/src/day11/demo3.java
@@ -0,0 +1,24 @@
+package day11;
+
+public class demo3 {
+
+	public static void main(String[] args) {
+		String str1 = "Hello";
+
+		String str2 = "Hello";
+
+		String s1 = new String("Hello");
+
+		String s2 = new String("Hello");
+
+		System.out.println(str1 == str2);
+
+		System.out.println(str2 == s1);
+
+		System.out.println(s1 == s2);
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
