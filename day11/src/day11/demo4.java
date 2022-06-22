4139864ce86b6c871733e1322172255af3178277
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-22 08:31:08 +0100

    day11 done

---
+++ day11/src/day11/demo4.java
@@ -0,0 +1,18 @@
+package day11;
+
+public class demo4 {
+
+	public static void main(String[] args) {
+		String s1 = new String("C2TC");
+
+		String s2 = new String("C2TC");
+
+		System.out.println(s1 == s2);
+
+		System.out.println(s1.equals(s2));
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
